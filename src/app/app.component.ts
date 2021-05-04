7c64d7df3d5d57d96c44f608fc1c4a7bb530aca2
Dallas James <dallastjames@gmail.com>
2021-05-04 14:17:19 -0600

    init to win it

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { AppMinimize } from '@ionic-native/app-minimize/ngx';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private am: AppMinimize,
+    private sb: StatusBar,
+    private ss: SplashScreen,
+    private plt: Platform
+  ) {
+    this.init();
+  }
+
+  public async init(): Promise<void> {
+    await this.plt.ready();
+    this.sb.styleDefault();
+    this.ss.hide();
+    this.plt.backButton.subscribeWithPriority(111, () => {
+      console.log('back button pressed');
+      this.am.minimize();
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,20 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SplashScreen } from '@ionic-native/splash-screen/ngx';
+import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { AppMinimize } from '@ionic-native/app-minimize/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    AppMinimize,
+    SplashScreen,
+    StatusBar,
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
